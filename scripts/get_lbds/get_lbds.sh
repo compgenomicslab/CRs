9b128b941a1c053f993ea8aed2f6c86f846bcc0f
csanchis <76164671+csanchis@users.noreply.github.com>
2021-09-02 13:18:17 +0200

    Update get_lbds.sh

--- scripts/get_lbds/get_lbds.sh
+++ scripts/get_lbds/get_lbds.sh
@@ -3,9 +3,8 @@
 #	1. TMHMM FILTERED (total.tmhmm.filtered)
 #	2. HMM SEARCH OF DOMAINS (params.total_pfam_seq2dom_info.tsv)
 
-cd ../../data/lbds/
-python ../../src/tmhmm_output.py total.tmhmm.filtered ../mcps.proteins/total.mcps.faa
-python ../../src/extract_fasta_hmm.py params.total_pfam_seq2dom_info.tsv ../mcps.proteins/total.mcps.faa
+python tmhmm_output.py total.tmhmm.filtered {fasta of CRs}
+python extract_fasta_hmm.py params.total_pfam_seq2dom_info.tsv {fasta of CRs}
 cat lbds_1tm_2tm.faa LBDs.faa > rebundant_lbds.faa
-python ../../src/add_fasta_id.py rebundant_lbds.faa LBDs.faa > LBDs_total.faa
+python add_fasta_id.py rebundant_lbds.faa LBDs.faa > LBDs_total.faa
 
